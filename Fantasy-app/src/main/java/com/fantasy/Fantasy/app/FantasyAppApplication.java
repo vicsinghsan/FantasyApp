d534b0bd51433033503645fdac68c5e1a0f29b50
Vivek kumar Singh <33048802+vicsinghsan@users.noreply.github.com>
2024-02-18 20:09:58 +0530

    Added the project directry

---
+++ Fantasy-app/src/main/java/com/fantasy/Fantasy/app/FantasyAppApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.Fantasy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyAppApplication.class, args);
+	}
+
+}
---
+++ Fantasy-app/src/test/java/com/fantasy/Fantasy/app/FantasyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.Fantasy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
